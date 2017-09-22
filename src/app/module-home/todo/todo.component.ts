8597c5fe7951e3624ede3c0efa218e63169defe0
sdlcldw <626289360@qq.com>
2017-09-22 16:37:49 +0800

    办公室提交

--- src/app/module-home/todo/todo.component.ts
+++ src/app/module-home/todo/todo.component.ts
@@ -11,16 +11,10 @@ export class TodoComponent implements OnInit {
     public todoList:Array<any>;
     public newTodoText:string = '';
     private _todoList = [
-      { text: 'Check me out' },
-      { text: 'Lorem ipsum dolor sit amet, possit denique oportere at his, etiam corpora deseruisse te pro' },
-      { text: 'Ex has semper alterum, expetenda dignissim' },
-      { text: 'Vim an eius ocurreret abhorreant, id nam aeque persius ornatus.' },
-      { text: 'Simul erroribus ad usu' },
-      { text: 'Ei cum solet appareat, ex est graeci mediocritatem' },
-      { text: 'Get in touch with akveo team' },
-      { text: 'Write email to business cat' },
-      { text: 'Have fun with blur admin' },
-      { text: 'What do you think?' },
+      { text: '示例：至少写1篇学校宣传稿件，并交年级主任审核。' },
+      { text: '示例：将我班级全部宿舍损坏的东西上报。' },
+      { text: '示例：给肖明飞家长打电话，过周末时将学生手机留在家中！' },
+
     ];
     constructor() {
       this.todoList = this._todoList;
