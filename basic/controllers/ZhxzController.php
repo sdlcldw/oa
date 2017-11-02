7847a214f29e04c4c75b104b657a4aaffce34418
sdlcldw <626289360@qq.com>
2017-11-02 16:42:41 +0800

    办公室上传

--- basic/controllers/ZhxzController.php
+++ basic/controllers/ZhxzController.php
@@ -14,6 +14,76 @@ class ZhxzController extends CommonController
 {
     public $enableCsrfValidation = false;// ->覆盖父类的属性
   
+    	// **上传考勤表方法**//
+	public function actionKqbsc_uploadfile(){
+		$objphpexcel = phpexcel_IOFactory::load($_FILES['kqb']['tmp_name']);
+		 $sheetCount=$objphpexcel->getSheetCount();
+		 // 取数据
+		 	$sheet = $objphpexcel->getSheet(0);
+			$highestRow = $sheet->getHighestRow(); // 取得总行数
+			$highestColumn = $sheet->getHighestColumn(); // 取得总列数
+
+			//**检查格式是否正确↓↓
+			$bt=array("考勤号码","姓名","日期","对应时段","上班时间","下班时间","签到时间","签退时间","迟到时间","早退时间","是否旷工","工作时间","例外情况","部门");
+			$zj=0;
+            foreach(range('A','N') as $v){
+			$bj = $sheet->getCell($v.'1')->getValue();	
+				if ($bj != $bt[$zj]){
+					return 1;
+				}
+					$zj++;
+			}
+			//检查本日期是否已上传
+			$rq = $sheet->getCell("C2")->getValue();
+			$sql = "select * from zhxz_kqb_mx where rq = '".$rq."'";
+			$dataReader=Yii::$app->db->createCommand($sql)->queryOne();
+			if($dataReader){
+				return 2;
+			}
+
+	for($y=2;$y<=$highestRow;$y++){	
+        $A=$sheet->getCell("A".$y)->getValue();
+        if(!$A){break;}
+		$B=$sheet->getCell("B".$y)->getValue();
+		// $usern=iconv('utf-8', 'gbk', $username);
+		$C = $sheet->getCell("C".$y)->getValue();
+		$D = $sheet->getCell("D".$y)->getValue();
+		$E = $sheet->getCell("E".$y)->getValue();
+		$F = $sheet->getCell("F".$y)->getValue();
+		$G = $sheet->getCell("G".$y)->getValue();
+		$H = $sheet->getCell("H".$y)->getValue();
+		$I = $sheet->getCell("I".$y)->getValue();
+		$J = $sheet->getCell("J".$y)->getValue();
+		$K = $sheet->getCell("K".$y)->getValue();
+		$L = $sheet->getCell("L".$y)->getValue();
+		$M = $sheet->getCell("M".$y)->getValue();
+		$N = $sheet->getCell("N".$y)->getValue();
+		$kqb[] = array("kqhm" => $A,"xm" => $B,"rq" => $C,"dysd" => $D,"sbsj" => $E,"xbsj" => $F,"qdsj" => $G,"qtsj" => $H,"cdsj" => $I,"ztsj" => $J,"sfkg" => $K,"gzsj" => $L,"lwqk" => $M,"bm" => $N);
+}
+		Yii::$app->session['kqb']= $kqb;
+		Yii::$app->response->format=Response::FORMAT_JSON;	 
+		return $kqb;		
+	}
+
+	public function actionKqbsc_sc(){
+		if (Yii::$app->request->isGet){
+				 	$kqbdata=Yii::$app->session['kqb'];
+                     unset(Yii::$app->session['kqb']);//删除该session
+				 	if(!$kqbdata){
+				 		return false; //在session获取上传数据失败
+				 	}
+				 	$sql = 'insert into zhxz_kqb_mx values ';
+					foreach($kqbdata as $key=>$v){
+		    				$sql.="('".$v['kqhm']."','".$v['xm']."','".$v['rq']."','".$v['dysd']."','".$v['sbsj']."','".$v['xbsj']."','".$v['qdsj']."','".$v['qtsj']."','".$v['cdsj']."','".$v['ztsj']."','".$v['sfkg']."','".$v['gzsj']."','".$v['lwqk']."','".$v['bm']."'),";
+						}
+						$sql = rtrim($sql,',');
+						$sql.=';';
+					
+						$data = Yii::$app->db->createCommand($sql)->execute();
+						Yii::$app->response->format=Response::FORMAT_JSON;	
+				 		return $data;
+		}
+	}
 
 
 
--- src/app/module-zhxz/kqbsc/kqbsc.component.ts
+++ src/app/module-zhxz/kqbsc/kqbsc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FileUploader} from "ng2-file-upload";
+import { Http,Headers } from "@angular/http";
 
 @Component({
   selector: 'app-kqbsc',
@@ -7,9 +9,71 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KqbscComponent implements OnInit {
 
-  constructor() { }
+  kqbdata:any = [];
+  // tggzdata:Array<any> = [];
+  
+  ifkqb:boolean = true;
+  yangli:boolean = false;
+  
+
+  constructor(private http:Http,) { }
 
   ngOnInit() {
+
   }
+
+// B: 初始化定义uploader变量,用来配置input中的uploader属性
+public uploader:FileUploader = new FileUploader({
+  url: "/oa/basic/web/index.php?r=zhxz/kqbsc_uploadfile",
+  method: "POST",
+  itemAlias: "kqb",
+  removeAfterUpload:true,
+});
+// C: 定义事件，选择文件
+selectedFileOnChanged(event:any) {
+  console.log(event.target.value);
+}
+// D: 定义事件，上传文件
+kqbyl() {
+  // 上传
+  // this.uploader.queue[0].onSuccess = function (response, status, headers) {
+    this.uploader.queue[0].onSuccess = (response, status, headers) => {    
+      // 上传文件成功
+      if (status == 200) {
+          // 上传文件后获取服务器返回的数据
+          // let tempRes = JSON.parse(response);
+          if(response == '1'){
+            alert("考勤表格式错误，请按照样例修改后再试！");
+            return; 
+          }
+        if(response=='2'){
+          alert("本月考勤表已存在，请勿重复上传！错误号：2");
+           return; 
+        }
+        this.yangli = true;
+        this.ifkqb = false; 
+        this.kqbdata = JSON.parse(response);
+
+      } else {
+          // 上传文件后获取服务器返回的数据错误
+          alert("上传失败了");
+      }
+  };
+  this.uploader.queue[0].upload(); // 开始上传
+}
+
+kqbsc(){
+  this.http.get('/oa/basic/web/index.php?r=zhxz/kqbsc_sc').subscribe((res=> {
+    console.log(res.json());
+    var data = res.json();
+    if(data){
+      alert('成功上传：'+ data + '条考勤记录！');
+      this.ifkqb = true;
+      this.yangli = false;
+    }
+}
+));
+}
+
 
 }
